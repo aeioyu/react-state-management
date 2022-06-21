9f011d71993bc8082cf7784f3ea620dc8986c8fe
Siripong Thieanchumphan <aeio.yu@gmail.com>
2022-06-21 19:33:57 +0700

    add jotai example

--- pages/jotai.tsx
+++ pages/jotai.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NextPage } from "next";
 import { atom, useAtom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
@@ -8,16 +8,61 @@ import { atomWithStorage } from "jotai/utils";
  */
 
 /** jotai atom */
-const textAtom = atom("hello");
+const exampleAtom = atom({
+  key: "text",
+  example: "Hello World",
+});
 
 /** jotai selector */
-const uppercaseAtom = atom((get) => get(textAtom).toUpperCase());
+const exampleAtomSelector = atom((get) => get(exampleAtom).example);
 
 /** jotai atom with localstorage */
 const darkModeAtom = atomWithStorage("darkMode", false);
 
 const Jotai: NextPage = () => {
-  return <div data-testid="Jotai">{/* page code */}</div>;
+  /** use atom */
+  const [text, setText] = useAtom(exampleAtom);
+  const handleSetText = () => {
+    setText((state) => ({
+      ...state,
+      example: `Hello World ${Math.random()}`,
+    }));
+  };
+
+  /** use selector */
+  const [exampleSelector] = useAtom(exampleAtomSelector);
+
+  /** use persistant atom */
+  const [darkMode, setDarkMode] = useAtom(darkModeAtom);
+  const toggleDarkMode = () => setDarkMode(!darkMode);
+
+  // prevent nextjs ssr
+  const [isSSR, setIsSSR] = useState(true);
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if (isSSR) return <></>;
+
+  return (
+    <div>
+      <h1>Jotai</h1>
+      <div>
+        <em>example atom: </em>
+        <code>{JSON.stringify(text)}</code>
+        <button onClick={handleSetText}>toggle</button>
+      </div>
+      <div>
+        <em>example selector: </em>
+        <code>{JSON.stringify(exampleSelector)}</code>
+      </div>
+      <div>
+        <em>persistant atom (localstorage): </em>
+        <code>{JSON.stringify(darkMode)}</code>
+        <button onClick={toggleDarkMode}>toggle</button>
+      </div>
+    </div>
+  );
 };
 
 export default Jotai;
